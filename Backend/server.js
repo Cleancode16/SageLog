1b21233cc3050112a9385967be386fafa4902164
Cleancode16 <devasanibharath6@gmail.com>
2025-03-08 18:13:15 +0530

    sage log is the blog app

---
+++ Backend/server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express=require('express');
+const { default: mongoose } = require('mongoose');
+const cors=require('cors')
+const app=express();
+
+//body parser middleware
+app.use(cors());
+app.use(express.json())
+const port=process.env.port||4000;
+mongoose.connect(process.env.DBURL).then(()=>app.listen(port,()=>
+  console.log("server listening at the "+port))).catch(e=>
+  console.log('the db is not connected '+e))
+  const {userApp}=require('../Backend/API/userAPI')
+  const {authorApp}=require('../Backend/API/authorApi')
+  const {adminApp}=require('../Backend/API/adminAPI')
+app.use('/user-api',userApp);
+app.use('/admin-api',adminApp);
+app.use('/author-api',authorApp);
+
+app.use((err,req,res,next)=>{
+  console.log('err object in express error handler:',err);
+  res.send({message:err.message});
+})
+
---
+++ Frontend/src/components/common/ArticleById.jsx
@@ -0,0 +1,270 @@
+import React, { useState } from 'react'
+import { useContext } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom'
+import { useForm } from 'react-hook-form';
+import { MdBlock } from "react-icons/md";
+import { MdComment } from "react-icons/md";
+import { UserAuthorContextObj } from '../../context/UserAuthorContext';
+import {FaEdit,FaSave} from 'react-icons/fa';
+import {MdDelete,MdRestore} from 'react-icons/md'
+import axios from 'axios';
+import {useAuth} from '@clerk/clerk-react'
+import { Navigate } from 'react-router-dom';
+import './header.css'
+function ArticleById() {
+const {register,handleSubmit,formState:{errors}}=useForm();
+const navigate=useNavigate();
+const {getToken}=useAuth();
+const {state}= useLocation();
+const {currentUser}=useContext(UserAuthorContextObj);
+const [currentArticle,setCurrentArticle]=useState(state);
+if(!state) return <p>Loading article...</p>
+
+  const [editArticleState, setEditArticleState]=useState(false)
+  function enableEdit() {
+    console.log(state.authorData.nameofauthor);
+    setEditArticleState(true);
+  }
+
+  async function onSave(modifiedObj) {
+    const articleAfterChanges={...state,...modifiedObj};
+    //add date of modification
+    const currentDate=new Date();
+    articleAfterChanges.dateofModification=currentDate.getDate()+"-"+currentDate.getMonth()+"-"+currentDate.getFullYear()+" "+ currentDate.toLocaleTimeString("en-US", {hour12:true});
+
+    //make http put rrequest
+    const token=await getToken();
+    let res=await axios.put(`http://localhost:3000/author-api/article/${articleAfterChanges.articleId}`,articleAfterChanges,{headers:{
+      Authorization:`Bearer ${token}`
+    }});
+    if(res.status===200){
+      //change edit article status to false
+      console.log('successfully updated the article');
+      navigate(`/author-profile/articles/${state.articleId}`,{state:res.data.payload});
+      setEditArticleState(false);
+    }
+
+  }
+  //delete article function
+  async function deleteArticle(){
+    state.isArticleActive=false;
+  let res=await axios.put(`http://localhost:3000/author-api/article/update/${state.articleId}`,state);
+  if(res.status==200){
+    console.log("successfully deleted");
+   setCurrentArticle(res.data.payload);
+  }
+  }
+  //restore Article
+  async function restoreArticle(){
+    console.log('restore');
+    state.isArticleActive=true;
+    let res=await axios.put(`http://localhost:3000/author-api/article/update/:articleId${state.articleId}`,state);
+    if(res.status==200){
+      console.log("successfully restored");
+      setCurrentArticle(res.data.payload);
+    }
+  }
+
+  // blocking article function
+  // async function blockArticle(){
+  //   state.isArticleActive=false;
+  //   let res=await axios.put(`http://localhost:3000/admin-api/block/:articleId${state.articleId}`,state);
+  //   if(res.status==200){
+  //     console.log("successfully blocked");
+  //     setCurrentArticle(res.data.payload);
+  //   }
+  // }
+  
+  {/* comment state */}
+  const [commentState, setCommentState] = useState('')
+
+    {/* add comment function */}
+    async function addComment(commentObj) {
+      console.log('comment obj', commentObj)
+      commentObj.nameofuser=currentUser.firstName;
+      commentObj.comment=commentObj.comments;
+      commentObj.commentId = Date.now();
+      let res = await axios.put(`http://localhost:3000/user-api/comment/${currentArticle.articleId}`, commentObj)
+      if(res.status === 200 ){
+        console.log("successfuly commented");
+      setCommentState(res.data.message)
+      }
+    }
+    console.log(currentUser.role);
+  return (
+    <div className='container'>
+      {
+        editArticleState===false ? 
+        <>
+          {/* print full article*/}
+          <div className="d-flex justify-content-between "> 
+              <div className="author-block w-100 px-4 py-2 rounded-2 d-flex justify-content-between align-items-center articlecard">
+                <div className='articleBanner'>
+                  <p className="fs-1 me-4">{state.title}</p>
+               
+                  {/* doc & dom */}
+                  <span className="py-3">
+                    <small className="text-light me-4">
+                      created on:{state.dateofCreation}
+                    </small>
+                    <small className="text-light me-4">
+                    Modified on:{state.dateofModification}
+                    </small>
+                  </span>
+                </div>
+                {/* author details */}
+                <div className="author-deatils text-center">
+                  <div className="mt-4">
+                    <img src={state.authorData.profileimageURL} width='60px' className='rounded-circle'alt="" />
+                    <p>{state.authorData.nameofauthor}</p>
+                  {
+                  (currentUser.role === 'author' && currentUser.firstName === state.authorData.nameofauthor)&&<> {/* edit & delete */}
+                  <button className='me-2 btn btn-light h-100 bg-secondary mt-5' height='40px' onClick={()=>enableEdit()}>
+                  <FaEdit className='text-warning'/>
+                  </button>
+                  {/* if article is active ,display delete icon,otherwise display restore icon */}
+
+                  {
+                  state.isArticleActive===true?(<button className="btn btn-light h-100 mt-5 bg-secondary" onClick={()=>deleteArticle()}>
+                  <MdDelete className='h-50 text-danger fs-4'/>
+                  </button>
+                  ):(<button className='btn btn-secondary h-100 mt-5' onClick={()=>restoreArticle()}>
+                  <MdRestore className=' m-2 text-info fs-4' />
+                  </button>)
+              }</>
+            }
+            </div>
+                </div>
+                
+            {
+              currentUser.role==='admin'&&<> {/* edit & delete */}
+           
+            {/* if article is active ,display delete icon,otherwise display restore icon */}
+
+            {
+              state.isArticleActive===true?(<button className='btn editRestoreButton btn-light bg-light ' onClick={()=>deleteArticle()}>
+                <MdBlock className=' m-2 text-info fs-4' />
+              </button>
+                 ):(<button className='btn editRestoreButton btn-light' onClick={()=>restoreArticle()}>
+                 <MdRestore className=' m-2   text-info fs-5' />
+               </button>)
+             }</>
+            }
+              </div>
+              
+            </div>
+
+            {/* contents */}
+            <p className="fs-5 mt-3 article-content bg-light p-3 rounded-3 paracontent" style={{whiteSpace:"pre-line"}}>{state.content}</p>
+            {/* user comments */}
+            <div>
+              <div className="comments">
+            <p className='fs-3 fw-bold'><MdComment className='fs-1 m-2' /> Comments</p>  
+            { 
+            state.comments.length===0?<p className='dispaly-3'>No Comments yet...</p>:
+            
+              state.comments.map((commentObj)=>{
+                return <div key={commentObj._id}>
+                         <div className="card m-2 p-3 bg-secondary">
+                         <p className='user-name text-light fw-semibold fs-5 '>
+                            {commentObj?.nameofuser}
+                          </p>
+                     
+                          <p className="comment text-light fs-6">
+                            {commentObj?.comment}
+                         </p></div> 
+                        
+                       
+
+                        </div>
+              }
+              )
+             
+            }
+          </div>
+        </div>
+        {/* comment form */}
+        {
+          <h1>{commentState}</h1>
+        }
+        {
+          currentUser.role === 'user' && <form onSubmit={handleSubmit(addComment)}>
+            <input type="text" id="comment" {...register('comments')} className="form-control mb-4" />
+            <button className='btn btn-success mb-3'>Add a comment</button>
+          </form>
+        }
+            
+        </>:
+        <>
+          {/* form  */}
+          <h1>Edit Article</h1>
+          <form onSubmit={handleSubmit(onSave)} >
+                <div className="mb-4">
+                  <label htmlFor="title" className="form-label">
+                    Title
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="title"
+                    defaultValue={state.title}
+                    {...register('title')}
+                 
+                  />
+                  {/* title validation err msg */}
+
+                </div>
+
+                <div className="mb-4">
+                  <label htmlFor="category" className="form-label">
+                    Select a category
+                  </label>
+                  <select
+               
+                    id="category"
+                    className="form-select"
+                    defaultValue={state.category}
+                    {...register('category')}
+                  >
+                    <option value="" disabled>--categories--</option>
+                    <option value="programming">Programming</option>
+                    <option value="AI&ML">AI&ML</option>
+                    <option value="database">Database</option>
+                  </select>
+                  {/* title validation err msg */}
+
+                </div>
+                <div className="mb-4">
+                  <label htmlFor="content" className="form-label">
+                    Content
+                  </label>
+                  <textarea
+                  
+                    className="form-control"
+                    id="content"
+                    rows="10"
+                    defaultValue={state.content}
+                    {...register('content')}
+                  ></textarea>
+                  {/* title validation err msg */}
+
+                </div>
+
+                <div className="text-end">
+                  <button type="submit" className=" btn btn-success add-article-btn">
+                    Post
+                  </button>
+                
+                </div>
+              </form>
+        </>
+      }
+      
+
+
+        
+    </div>
+  )
+}
+
+export default ArticleById
